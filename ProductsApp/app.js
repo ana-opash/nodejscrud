c1b43325e12b4f7466598250bdc6e249d6d1d759
Hiren Danecha <95602699+ana-opash@users.noreply.github.com>
2022-09-26 15:02:48 +0530

    make it better

---
+++ ProductsApp/app.js
@@ -0,0 +1,39 @@
+// app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//Imports routes 
+const product = require('./routes/product.route');
+
+// initialize our express app
+const app = express();
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb+srv://ana_opash:ana_opash@cluster0.ytiubnl.mongodb.net/?retryWrites=true&w=majority'
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+mongoose.connect(mongoDB)
+    .then(() => {
+        console.log("Connected to MongoDB Successfully.");
+    })
+    .catch((err) => {
+        console.log("Could not connect to MongoDB");
+        console.log("err");
+    });
+mongoose.Promise = global.Promise;
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/products', product);
+
+// create our dedicating a port and telling our express app
+let port = 1234;
+app.listen(port, () => {
+    console.log('Server is up and running on port number' + port);
+});
